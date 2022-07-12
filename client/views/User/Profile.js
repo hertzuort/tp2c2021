53eee79760fbab9bcd00762244df0641599747dd
Ezequiel Hertzulis <ehertzulis@proyecto404.com>
2022-07-12 17:01:22 -0300

    UserProfile

--- client/views/User/Profile.js
+++ client/views/User/Profile.js
@@ -2,40 +2,40 @@ import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import {Loader} from "../Components/Loader/Loader";
 import styled from 'styled-components/native';
+import {Header} from "../Header";
 
 export default function Profile({ navigation }) {
-    const SERVER_URL = "http://localhost:3001";
-
     const [info, setInfo] = React.useState();
 
-    React.useEffect(() => {
-        fetch(`${SERVER_URL}/api/users/6259b5848de6a57670adb45b`)
-            .then(respuesta => respuesta.json())
-            .then(respuestaJson => setInfo(respuestaJson))
-            .catch(error => {
-                console.error(error);
-            });
-    }, [info]);
+    React.useState(() => {
+        const session = localStorage.getItem('session');
+        setInfo(JSON.parse(session));
+    });
 
     return (
         <Container>
-            <Card>
-                <Title>Perfil</Title>
-                <Info>
-                    <Row>
-                        <Key>Nombre: </Key>
-                        <Value>nombre </Value>
-                    </Row>
-                    <Row>
-                        <Key>Apellido: </Key>
-                        <Value>apellido </Value>
-                    </Row>
-                    <Row>
-                        <Key>Email: </Key>
-                        <Value>email </Value>
-                    </Row>
-                </Info>
-            </Card>
+            <Header navigation={navigation} />
+            {
+                info ?
+                    <Card>
+                        <Title>Perfil</Title>
+                        <Info>
+                            <Row>
+                                <Key>Nombre:</Key>
+                                <Value>{info.name}</Value>
+                            </Row>
+                            <Row>
+                                <Key>Apellido:</Key>
+                                <Value>{info.lastName}</Value>
+                            </Row>
+                            <Row>
+                                <Key>Email:</Key>
+                                <Value>{info.email}</Value>
+                            </Row>
+                        </Info>
+                    </Card> :
+                    <Loader />
+            }
         </Container>
       );
 }
